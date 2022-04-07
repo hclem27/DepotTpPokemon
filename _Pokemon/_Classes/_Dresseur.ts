5fd7b14812863e06dd6b7ad31c235ec11f3fa2b0
hclem27 <97625142+hclem27@users.noreply.github.com>
2022-04-07 09:57:47 +0200

    Add files via upload

--- _Pokemon/_Classes/_Dresseur.ts
+++ _Pokemon/_Classes/_Dresseur.ts
@@ -5,6 +5,7 @@ export default class Dresseur {
 // Variables : 
 private nom: string;
 public pokeballs!: Pokeball[];
+
 // Constructeur
 constructor(nom: string){
     this.nom = nom;
@@ -16,7 +17,7 @@ get nbPokeballs(){
 // Methodes :
 
 // On ajoute 1 ou plus pokeballs, dans un maximum de 6 !
-ajouterPokeballs(pokeball: Pokeball): void{
+public ajouterPokeballs(pokeball: Pokeball): void{
     if (this.pokeballs.length < 6){
         this.pokeballs.push(pokeball)
     } else {
@@ -27,23 +28,54 @@ ajouterPokeballs(pokeball: Pokeball): void{
 // Vérifie si le dresseur a encore des pokeballs dispo
 private getPokeballs(): boolean{
     if (this.pokeballs.length == 6){
+        console.log("Vous avez déjà 6 pokeballs")
         return false
     } else {
+        console.log("Tu as encore des pokeballs de dispo")
         return true
     }
 }
+
 // affecte un pokemon à une pokeball, et précise son maitre au passage
 private fillEmptyPokeball(Pokemon: Pokemon, dresseur: Dresseur): void{
+    if (this.pokeballs.length == 6){
+        console.log("Vous ne pouvez affecter un pokemon à une pokeball, vous avez déjà 6 pokeball")
+    } else {
+        this.pokeballs.push()
+
+    }
 
 }
 // Permet de vérifier si un pokemon n'a pas déjà été capturé,
 // Appelle 'fillEmptyPokeball'
-capturer(cible: Pokemon): void{
-
+public capturer(pokemon: Pokemon){
+    if(this.pokeballs.length == 0){
+        console.log("Vous n'avez pas de pokeballs")
+    }
+    for(let i = 0; i < this.pokeballs.length; i++){
+        if(this.pokeballs[i].isEmpty()){
+            this.pokeballs[i].affecterPokemon(pokemon)
+            return
+        }
+    }
 }
 // Affiche les pokemons contenus dans chage pokeball
 public getPokemons(): void{
-    console.log(this.pokeballs.forEach(Pokeball => console.log(Pokeball)));    
+    if(this.pokeballs.length > 0){
+        console.log("Vos pokemons")
+        this.pokeballs.forEach((pokeball,i) => {
+            i++
+            if(pokeball.getContenu()){
+                console.log(`Pokeball ${i} ${pokeball.getContenu().nom}`)
+                console.log(pokeball.getContenu())
+            }else{
+                console.log(`Pokeball ${i} : Vide`)
+            }
+
+        })
+    }else{
+        console.log("Vous n'avez pas de pokémons dans vos pokeballs")
+    }    
 }
 
 }
--- _Pokemon/_Classes/_Pokeball.ts
+++ _Pokemon/_Classes/_Pokeball.ts
@@ -2,6 +2,7 @@ import Pokemon from "../_Interfaces/_Pokemon";
 import Dresseur from "../_Classes/_Dresseur";
 
 export default class Pokeball {
+    [x: string]: any;
 
     private contient!: Pokemon
     private proprietaire!: Dresseur
